dab2b8c2b43b035afa22cc45d6936a3ba5aa6b6c
maami415 <maami.sakamoto@gmail.com>
2022-10-01 15:27:53 +0900

    セクション4の実装

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root "articles#index"
+  # 1行開ける必要がある
+  get "/articles", to: "articles#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
